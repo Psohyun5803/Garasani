153ed713e05224ad5bdf2d4f95e117d59049e784
yeonee <155801470+y-eonee@users.noreply.github.com>
2024-04-29 21:32:03 +0900

    button 추가

--- Assets/Script/PhonUI.cs
+++ Assets/Script/PhonUI.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
+    public GameObject ui_Phone;
+    public GameObject ui_TwitterIcon;
+    public GameObject ui_WikiIcon;
+    public GameObject ui_TodoIcon;
+    public GameObject ui_GalleryIcon;
+    public GameObject ui_CallIcon;
+    public GameObject ui_MessageIcon;
+
+   
     // Start is called before the first frame update
     void Start()
     {

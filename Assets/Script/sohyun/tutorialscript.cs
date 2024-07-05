d56bcb967b47300a79ad4edb50cc3687c107e821
sohyun-1122 <134932184+sohyun-1122@users.noreply.github.com>
2024-07-05 17:49:51 +0900

    말풍선/상호작용

--- Assets/Script/sohyun/tutorialscript.cs
+++ Assets/Script/sohyun/tutorialscript.cs
@@ -56,43 +56,45 @@ public class tutorialscript : MonoBehaviour
             말풍선.SetActive(false);
         }
     }
- /*   private void OnMouseOver() //아이템 위에 커서 있는 것 감지 
-    {
-        if (!isMouseOver) //커서가 오브젝트 위에 올라갔을 때 최초 1번만 실행
-        {
-            isMouseOver = true;
-            string objectName = gameObject.name; //커서 감지한 오브젝트 이름 
-            Debug.Log("마우스 감지" + objectName);
+    /*   private void OnMouseOver() //아이템 위에 커서 있는 것 감지 
+       {
+           if (!isMouseOver) //커서가 오브젝트 위에 올라갔을 때 최초 1번만 실행
+           {
+               isMouseOver = true;
+               string objectName = gameObject.name; //커서 감지한 오브젝트 이름 
+               Debug.Log("마우스 감지" + objectName);
 
-            Vector3 mousePosition = Input.mousePosition; //커서 좌표 가져옴 
-            worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, Camera.main.nearClipPlane));
+               Vector3 mousePosition = Input.mousePosition; //커서 좌표 가져옴 
+               worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, Camera.main.nearClipPlane));
 
-            ActiveInteraction(); //상태창 on
-        }
+               ActiveInteraction(); //상태창 on
+           }
 
-    }
-    void OnMouseExit()
-    {
-        // 마우스가 오브젝트에서 벗어났을 때 플래그를 리셋
-        isMouseOver = false;
-        InteractionView.SetActive(false);
-    }
+       }
+       void OnMouseExit()
+       {
+           // 마우스가 오브젝트에서 벗어났을 때 플래그를 리셋
+           isMouseOver = false;
+           InteractionView.SetActive(false);
+       }
 
 
 
-    public void ActiveInteraction()
-    {
-        InteractionView.transform.position = (worldPosition); //오브젝트 커서 위치로 상태창 이동 
-        InteractionView.SetActive(true); //커서 감지 시 상태창 on
-        if(gameObject.name=="찢겨진부적")
-        {
-            objectcounter = 0;
-            설명.text = 상호작용[objectcounter];
-        }
-    }
- */
-    
+       public void ActiveInteraction()
+       {
+           InteractionView.transform.position = (worldPosition); //오브젝트 커서 위치로 상태창 이동 
+           InteractionView.SetActive(true); //커서 감지 시 상태창 on
+           if(gameObject.name=="찢겨진부적")
+           {
+               objectcounter = 0;
+               설명.text = 상호작용[objectcounter];
+           }
+       }
+    */
+
     // Update is called once per frame
+    int clickflag = 0;
+    
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftShift))
@@ -105,6 +107,58 @@ public class tutorialscript : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             말풍선.SetActive(false);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+            if (hit.collider != null)
+            {
+                GameObject clickobj = hit.transform.gameObject;
+                if (clickflag == 0 && clickobj.name == "열차사이문")
+                {
+                    
+                       
+                            말풍선.SetActive(true);
+                            이름.text = "System";
+                            내용.text = "문이 열렸다";
+                            clickflag = 1;
+                }
+                else if (clickflag==1&& clickobj.name == "열차사이문")
+                {
+                   
+                        말풍선.SetActive(true);
+                        이름.text = "System";
+                        내용.text = "다음 칸으로 이동한다";
+                        clickflag = 2;
+                    
+                }
+                else if (clickobj.name=="찢겨진부적")
+                {
+                    말풍선.SetActive(true);
+                    이름.text = "System";
+                    내용.text = "[찢겨진 부적] : 영문을 알 수 없는 글씨가 쓰여진 종이. 섬뜩하게 찢겨져있다. ";
+                    clickobj.SetActive(false);
+                }
+                else if (clickobj.name == "에어팟한쪽")
+                {
+                    말풍선.SetActive(true);
+                    이름.text = "System";
+                    내용.text = "[누군가 두고 내린 에어팟 한쪽]을 가방에 챙겼다";
+                    clickobj.SetActive(false);
+                }
+                else if (clickobj.name== "키링")
+                {
+                    말풍선.SetActive(true);
+                    이름.text = "System";
+                    내용.text = "[누군가 흘린 키링]을 가방에 챙겼다.";
+                    clickobj.SetActive(false);
+                }
+
+
+
+            }
         }
     }
 }

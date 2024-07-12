7adc377e41bf95f1fdd55d9673b966db21646cfa
yeonee <155801470+y-eonee@users.noreply.github.com>
2024-07-13 01:12:00 +0900

    dialoguemanager 모듈화
    * 씬 부분과 대화 디스플레이하는 부분을 따로 스크립트로 분리
    * 백틱 쉼표로 replacce 해야함
    * 선택지 text가 안눌림

---
+++ Assets/Script/nayeon/NPC Dialogue/Dialogue/DialogueManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager instance;
+    public TMP_Text dialogue_text;
+    public TMP_Text name;
+    public TMP_Text chosen1_text;
+    public TMP_Text chosen2_text;
+    public int currentIdx;
+    public int ChooseFlag = 0;
+    public  Dialogue[] contextList;
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void Initialize(Dialogue[] dialogues)
+    {
+        contextList = dialogues;
+        currentIdx = 0;
+        DisplayDialogue(contextList);
+    }
+
+    public void onClickNextButton()
+    {
+        if (contextList != null && currentIdx < contextList.Length - 1)
+        {
+            currentIdx++; // 다음 문장으로 이동
+            DisplayDialogue(contextList);
+        }
+        else
+        {
+            Debug.Log("contextlist 초기화 안됨");
+        }
+    }
+
+    public void OnClickChoose()
+    {
+        //태그가 1이면 번호 1리턴, 2면 2리턴
+        if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen1") == 0)
+            ChooseFlag = 1;
+        else ChooseFlag = 2;
+
+    }
+
+    public void DisplayDialogue(Dialogue[] dialogues)
+    {
+        contextList = dialogues;
+        if (contextList == null || contextList.Length == 0 || currentIdx >= contextList.Length)
+            return;
+
+        dialogue_text.text = contextList[currentIdx].contexts;
+        name.text = contextList[currentIdx].name;
+
+        if (!string.IsNullOrEmpty(contextList[currentIdx].chosen1))
+        {
+            chosen1_text.text = contextList[currentIdx].chosen1;
+            chosen2_text.text = contextList[currentIdx].chosen2;
+        }
+        else
+        {
+            chosen1_text.text = "";
+            chosen2_text.text = "";
+        }
+    }
+
+}
--- Assets/Script/nayeon/NPC Dialogue/Prologue2Dialogue.cs
+++ Assets/Script/nayeon/NPC Dialogue/Prologue2Dialogue.cs
@@ -7,15 +7,7 @@ using UnityEngine.EventSystems;
 public class Prologue2Dialogue : MonoBehaviour
 {
     public static Prologue2Dialogue instance;
-    public TMP_Text dialogue_text;
-    public TMP_Text name;
-    public TMP_Text chosen1_text;
-    public TMP_Text chosen2_text;
-    int currentIdx;
-    string ChooseFlag = "";
     public int dialogueID;
-
-    private Dialogue[] contextList; // 대화 내용을 저장하는 배열
 
     public void Awake()
     {
@@ -27,104 +19,50 @@ public class Prologue2Dialogue : MonoBehaviour
 
     public void prologue2()
     {
-        while (dialogueID < 4)
+        dialogueID = 1;
+        while (dialogueID < 3)
         {
-            dialogueID = 1;
-
             switch (dialogueID)
             {
                 case (1):
-                    pro_id1(DataManager.instance.GetDialogue(1, 7));
+                    pro_id1(DataManager.instance.GetDialogue(1,7));
                     break;
                 case (2):
                     pro_id2(DataManager.instance.GetDialogue(8, 8));
                     break;
                 case (3):
-                    pro_id2(DataManager.instance.GetDialogue(9, 9));
+                    pro_id3(DataManager.instance.GetDialogue(9, 9));
+                    break;
+                default:
                     break;
             }
-        }
-        
-    }
-    public void onClickNextButton()
-    {
-        if (contextList != null && currentIdx < contextList.Length - 1)
-        {
-            currentIdx++; // 다음 문장으로 이동
-            DisplayDialogue();
-        }
-        else
-        {
-            Debug.Log("contextlist 초기화 안됨");
+
         }
     }
 
-    public void OnClickChoose()
-    {
-        //태그가 1이면 번호 1리턴, 2면 2리턴
-        ChooseFlag = EventSystem.current.currentSelectedGameObject.tag;
-        
-    }
 
-
-    private void DisplayDialogue()
-    {
-        if (contextList == null || contextList.Length == 0 || currentIdx >= contextList.Length)
-            return;
-
-        dialogue_text.text = contextList[currentIdx].contexts;
-        name.text = contextList[currentIdx].name;
-
-        if (!string.IsNullOrEmpty(contextList[currentIdx].chosen1))
-        {
-            chosen1_text.text = contextList[currentIdx].chosen1;
-            chosen2_text.text = contextList[currentIdx].chosen2;
-        }
-        else
-        {
-            chosen1_text.text = "";
-            chosen2_text.text = "";
-        }
-
-
-    }
 
 
     public void pro_id1(Dialogue[] dialogues)
     {
-        contextList = dialogues; // contextList 초기화
-        currentIdx = 0; // 인덱스 초기화
-        DisplayDialogue();
-        if (ChooseFlag.CompareTo("chosen1") == 0)
+        DialogueManager.instance.DisplayDialogue(dialogues);
+        if (DialogueManager.instance.ChooseFlag == 1)
             dialogueID = 2;
         else dialogueID = 3;
     }
 
     public void pro_id2(Dialogue[] dialogues)
     {
-        contextList = dialogues; // contextList 초기화
-        currentIdx = 0; // 인덱스 초기화
-        DisplayDialogue();
+        DialogueManager.instance.DisplayDialogue(dialogues);
         Debug.Log("id2");
     }
+
     public void pro_id3(Dialogue[] dialogues)
     {
-        contextList = dialogues; // contextList 초기화
-        currentIdx = 0; // 인덱스 초기화
-        DisplayDialogue();
+        DialogueManager.instance.DisplayDialogue(dialogues);
         Debug.Log("id3");
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }

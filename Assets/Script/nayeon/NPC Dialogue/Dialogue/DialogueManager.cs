8f0af5b39314c91ee69e821e4fa03f9d1a574f52
yeonee <155801470+y-eonee@users.noreply.github.com>
2024-07-27 01:09:21 +0900

    대화 수정사항

--- Assets/Script/nayeon/NPC Dialogue/Dialogue/DialogueManager.cs
+++ Assets/Script/nayeon/NPC Dialogue/Dialogue/DialogueManager.cs
@@ -11,12 +11,17 @@ public class DialogueManager : MonoBehaviour
     public TMP_Text name;
     public TMP_Text chosen1_text;
     public TMP_Text chosen2_text;
-    public TMP_Text chosen3_text;
+
     public int currentIdx;
     public bool IsDialogueFinished;
     public  Dialogue[] contextList;
-    public int chooseFlag = 0;
-    public bool clickFlag = false;
+    public int chooseFlag = 0; //선택지 2개인 경우 flag값 
+    public bool clickFlag = false; //선택지가 1개인경우 클릭 확인 
+    private bool isChosenOne = false; //선택지가 1개인경우
+
+    private float delay = 0.05f; //타이핑 속도
+    private Coroutine typingCoroutine;
+    private bool isTyping = false;
 
     public void Awake()
     {
@@ -24,6 +29,7 @@ public class DialogueManager : MonoBehaviour
         {
             instance = this;
         }
+        
     }
 
     public void Initialize(Dialogue[] dialogues)
@@ -50,46 +56,89 @@ public class DialogueManager : MonoBehaviour
 
     public void DisplayDialogue()
     {
+        isTyping = true;
         if (contextList == null || contextList.Length == 0 || currentIdx >= contextList.Length)
             return;
 
-        dialogue_text.text = contextList[currentIdx].contexts;
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+            typingCoroutine = null;
+        }
+
+
+        dialogue_text.text = "";
+        chosen1_text.text = "";
+        chosen2_text.text = "";
         name.text = contextList[currentIdx].name;
 
-        if (!string.IsNullOrEmpty(contextList[currentIdx].chosen1) && string.IsNullOrEmpty(contextList[currentIdx].chosen3))
+        if(name.text != customize.playername) //주인공과 npc 왼쪽,오른쪽 정렬 구분 
+        {
+            dialogue_text.alignment = TextAlignmentOptions.Right;
+        }
+        else
+        {
+            dialogue_text.alignment = TextAlignmentOptions.Left;
+        }
+
+        typingCoroutine = StartCoroutine(textPrint(delay, contextList[currentIdx].contexts));
+        
+    }
+
+    IEnumerator textPrint(float d, string text)
+    {
+        int count = 0;
+
+        while (count != text.Length)
+        {
+            if (count < text.Length)
+            {
+                dialogue_text.text += text[count].ToString();
+                count++;
+            }
+
+            yield return new WaitForSeconds(delay);
+        }
+
+        isTyping = false;
+        ShowChoices();
+    }
+    private void ShowChoices()
+    {
+        if (!string.IsNullOrEmpty(contextList[currentIdx].chosen1) && !string.IsNullOrEmpty(contextList[currentIdx].chosen2))
         {
             chosen1_text.text = contextList[currentIdx].chosen1;
             chosen2_text.text = contextList[currentIdx].chosen2;
-            chosen3_text.text = "";
+            isChosenOne = false;
         }
-        else if(!string.IsNullOrEmpty(contextList[currentIdx].chosen3) && string.IsNullOrEmpty(contextList[currentIdx].chosen1))
+        else if (!string.IsNullOrEmpty(contextList[currentIdx].chosen1) && string.IsNullOrEmpty(contextList[currentIdx].chosen2))
+        {
+            chosen1_text.text = "";
+            chosen2_text.text = contextList[currentIdx].chosen1;
+            isChosenOne = true;
+        }
+        else
         {
             chosen1_text.text = "";
             chosen2_text.text = "";
-            chosen3_text.text = contextList[currentIdx].chosen3;
-
         }
-        else if(string.IsNullOrEmpty(contextList[currentIdx].chosen1) && string.IsNullOrEmpty(contextList[currentIdx].chosen3))
-        {
-            chosen1_text.text = "";
-            chosen2_text.text = "";
-            chosen3_text.text = "";
-        }
- 
     }
 
     public void OnClickChoose()
     {
         Debug.Log("선택지 클릭 확인");
         //태그가 1이면 번호 1리턴, 2면 2리턴
-        if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen1") == 0)
-            chooseFlag = 1;
-        else if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen2") == 0)
-            chooseFlag = 2;
-        else if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen3") == 0)
+        if (isChosenOne)
             clickFlag = true;
-        Debug.Log("chooseFlag: " + chooseFlag + ", clickFlag: " + clickFlag);
+        else
+        {
+            if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen1") == 0)
+                chooseFlag = 1;
+            else if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen2") == 0)
+                chooseFlag = 2;
+        }
 
+        Debug.Log(clickFlag);
     }
 
   
@@ -106,4 +155,21 @@ public class DialogueManager : MonoBehaviour
 
     }
 
+    void Update()
+    {
+        // 마우스 클릭으로 타이핑효과 코루틴 멈추고 대사 즉시 출력
+        if (isTyping && EventSystem.current.currentSelectedGameObject != null && EventSystem.current.currentSelectedGameObject.name == "말풍선")
+        {
+            if (typingCoroutine != null)
+            {
+                StopCoroutine(typingCoroutine);
+                typingCoroutine = null;
+            }
+
+            dialogue_text.text = contextList[currentIdx].contexts;
+            isTyping = false;
+
+            ShowChoices(); // 즉시 대사 출력 후 선택지 출력
+        }
+    }
 }

b2594cee3f7f5ece6de0dca9ee135534e4f398db
yeonee <155801470+y-eonee@users.noreply.github.com>
2024-07-27 01:05:05 +0900

    스토리 진행 오류 수정

--- Assets/Script/nayeon/NPC Dialogue/Dialogue/DialogueManager.cs
+++ Assets/Script/nayeon/NPC Dialogue/Dialogue/DialogueManager.cs
@@ -15,10 +15,11 @@ public class DialogueManager : MonoBehaviour
     public int currentIdx;
     public bool IsDialogueFinished;
     public  Dialogue[] contextList;
-    public int chooseFlag = 0;
-    public bool clickFlag = false;
+    public int chooseFlag = 0; //선택지 2개인 경우 flag값 
+    public bool clickFlag = false; //선택지가 1개인경우 클릭 확인 
+    private bool isChosenOne = false; //선택지가 1개인경우
 
-    private float delay = 0.05f;
+    private float delay = 0.05f; //타이핑 속도
     private Coroutine typingCoroutine;
     private bool isTyping = false;
 
@@ -71,7 +72,7 @@ public class DialogueManager : MonoBehaviour
         chosen2_text.text = "";
         name.text = contextList[currentIdx].name;
 
-        if(name.text != customize.playername)
+        if(name.text != customize.playername) //주인공과 npc 왼쪽,오른쪽 정렬 구분 
         {
             dialogue_text.alignment = TextAlignmentOptions.Right;
         }
@@ -79,26 +80,9 @@ public class DialogueManager : MonoBehaviour
         {
             dialogue_text.alignment = TextAlignmentOptions.Left;
         }
-        //dialogue_text.text = contextList[currentIdx].contexts;
+
         typingCoroutine = StartCoroutine(textPrint(delay, contextList[currentIdx].contexts));
         
-
-        //if (!string.IsNullOrEmpty(contextList[currentIdx].chosen1) && !string.IsNullOrEmpty(contextList[currentIdx].chosen2))
-        //{
-        //    chosen1_text.text = contextList[currentIdx].chosen1;
-        //    chosen2_text.text = contextList[currentIdx].chosen2;
-        //}
-        //else if(!string.IsNullOrEmpty(contextList[currentIdx].chosen1) && string.IsNullOrEmpty(contextList[currentIdx].chosen2))
-        //{
-        //    chosen1_text.text = contextList[currentIdx].chosen1;
-        //    chosen2_text.text = "";
-
-        //}
-        //else
-        //{
-        //    chosen1_text.text = "";
-        //    chosen2_text.text = "";
-        //}
     }
 
     IEnumerator textPrint(float d, string text)
@@ -125,11 +109,13 @@ public class DialogueManager : MonoBehaviour
         {
             chosen1_text.text = contextList[currentIdx].chosen1;
             chosen2_text.text = contextList[currentIdx].chosen2;
+            isChosenOne = false;
         }
         else if (!string.IsNullOrEmpty(contextList[currentIdx].chosen1) && string.IsNullOrEmpty(contextList[currentIdx].chosen2))
         {
-            chosen1_text.text = contextList[currentIdx].chosen1;
-            chosen2_text.text = "";
+            chosen1_text.text = "";
+            chosen2_text.text = contextList[currentIdx].chosen1;
+            isChosenOne = true;
         }
         else
         {
@@ -142,11 +128,21 @@ public class DialogueManager : MonoBehaviour
     {
         Debug.Log("선택지 클릭 확인");
         //태그가 1이면 번호 1리턴, 2면 2리턴
-        if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen1") == 0)
-            chooseFlag = 1;
-        else if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen2") == 0)
-            chooseFlag = 2;
+        if (isChosenOne)
+            clickFlag = true;
+        else
+        {
+            if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen1") == 0)
+                chooseFlag = 1;
+            else if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen2") == 0)
+                chooseFlag = 2;
+        }
 
+        //if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen1") == 0)
+        //    chooseFlag = 1;
+        //else if (EventSystem.current.currentSelectedGameObject.tag.CompareTo("chosen2") == 0)
+        //    chooseFlag = 2;
+        Debug.Log(clickFlag);
     }
 
   
@@ -165,7 +161,7 @@ public class DialogueManager : MonoBehaviour
 
     void Update()
     {
-        // 마우스 클릭으로 대사 즉시 출력
+        // 마우스 클릭으로 타이핑효과 코루틴 멈추고 대사 즉시 출력
         if (isTyping && EventSystem.current.currentSelectedGameObject != null && EventSystem.current.currentSelectedGameObject.name == "말풍선")
         {
             if (typingCoroutine != null)

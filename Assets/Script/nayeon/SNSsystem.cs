a62cef37fcc905835b6bfa10b648fdae66a03c28
yeonee <leenayeon@sookmyung.ac.kr>
2024-08-10 01:50:44 +0900

    sns시스템

---
+++ Assets/Script/nayeon/SNSsystem.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SNSsystem : MonoBehaviour
+{
+
+    public GameObject ch3_market;
+    public GameObject ch3_home;
+    public GameObject ch3_weather;
+    public GameObject ch3_movie;
+    public GameObject ch3_horror;
+
+    public void SNSmanager() //챕터 시작할 때마다 각 스크립트에 함수 삽입
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        switch (scene.name)
+        {
+            case "PhoneUI":
+                ch3_home.SetActive(true);
+                ch3_market.SetActive(true);
+                ch3_weather.SetActive(true);
+                ch3_movie.SetActive(true);
+                ch3_horror.SetActive(true);
+                break;
+            case "Jongro3":
+                break;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ch3_home.SetActive(false);
+        ch3_market.SetActive(false);
+        ch3_weather.SetActive(false);
+        ch3_movie.SetActive(false);
+        ch3_horror.SetActive(false);
+        //SNSmanager();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

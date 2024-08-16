eb85a40656588693bef39baaa87a9c4dea0b3250
sohyun-1122 <134932184+sohyun-1122@users.noreply.github.com>
2024-08-16 18:34:40 +0900

    선택지 버그 수정필요

--- Assets/Script/sohyun/npc.cs
+++ Assets/Script/sohyun/npc.cs
@@ -10,6 +10,7 @@ using TMPro;
 using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
 using System;
+//"구매했다"가 들어가는 곳에 자원 차감하면 됨
 
 public class npc : MonoBehaviour
 {
@@ -19,7 +20,14 @@ public class npc : MonoBehaviour
     public TMP_Text who;
     public TMP_Text option1;
     public TMP_Text option2;
+    public TMP_Text option3;
+    //
+    //public TMP_Text exit;
+
     public GameObject options;
+    public GameObject option3_bt;
+    public static int clofirst = 0;
+    public static int manjufirst = 0;
     public static int optnum = 0;
     public static string interobj;
     public static int buttonnum = 0;
@@ -30,6 +38,8 @@ public class npc : MonoBehaviour
     string[] objcontent = new string[1] { "................." };
     string[] angercontent = new string[3] { "어이,거기!", "눈을 어따 두고 다니는 거야?", "쯧." };
     string[] manjucontent = new string[4] {"헤헤,정말 괜찮은데...","정민네.","......","정민....대학생이 무슨 돈이 있겠어요~"};
+    string[] clocontent = new string[3] { "정민오, 패션에 관심 있으신가봐요? 저돈데!","정민이것도 사실 지하상가에서 산 거거든요~","정민여기 질 괜찮다니까?" };
+
     //string[] jihoonfirst = new string[] = {"정민어, 안녕?","지훈으아아아아아앙!!","정민엄마랑 아빠는 어디가셨어?","지훈몰라...엄마아아...","지훈엄마가 안 보여... 끅....","정민미아같은데...어떡할까요?","선1이름을 물어본다","선2먹을 것을 건넨다")
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -49,13 +59,53 @@ public class npc : MonoBehaviour
         
         option1.text = "";
         option2.text = "";
+        if (option3_bt!=null&&option3_bt.activeSelf)
+        {
+            if (option3 != null)
+            {
+                option3.text = "";
+            }
+        }
         if (interobj=="델리만쥬 가게")
         {
-            who.text = "Player";
-            content.text = "사 줄까요?";
+            if(manjufirst!=1)
+            {
+                who.text = "Player";
+                content.text = "사 줄까요?";
+                button.SetActive(true);
+                buttonnum = 0;
+
+            }
+            else
+            {
+                who.text = "system";
+                content.text = "델리만쥬를 구매했다.";
+            }
+            
+            
+
+        }
+
+        if (interobj == "옷 가게")
+        {
+            
+            
+             who.text = "system";
+             content.text = "짱구 잠옷을 구매했다.";
+             button.SetActive(true);
+             buttonnum = 0;
+
+
+
+        }
+
+        if (interobj == "편의점")
+        {
+            who.text = "system";
+            content.text = "물을 구매했다.";
             button.SetActive(true);
             buttonnum = 0;
-            
+
 
         }
         options.SetActive(false);
@@ -67,16 +117,104 @@ public class npc : MonoBehaviour
         optnum = 2;
         option1.text = "";
         option2.text = "";
+        if(option3_bt != null && option3_bt.activeSelf)
+        {
+            if (option3 != null)
+            {
+                option3.text = "";
+            }
+            
+        }
+      
         if (interobj == "델리만쥬 가게")
         {
-            who.text = "Player";
-            content.text = "돈 없으세요?";
+            if(manjufirst==0)
+            {
+                who.text = "Player";
+                content.text = "돈 없으세요?";
+                button.SetActive(true);
+                buttonnum = 1;
+
+            }
+            else
+            {
+                who.text = "system";
+                content.text = "핫도그를 구매했다.";
+            }
+           
+
+        }
+        if (interobj == "옷 가게")
+        {
+            who.text = "system";
+            content.text = "요상한 맨투맨을 구매했다";
             button.SetActive(true);
-            buttonnum = 1;
+            buttonnum = 0;
+
+
+        }
+        if (interobj == "편의점")
+        {
+            who.text = "system";
+            content.text = "보조배터리를 구매했다.";
+            button.SetActive(true);
+            buttonnum = 0;
+
 
         }
         options.SetActive(false);
     }
+    public void option3down()
+    {
+        optnum = 3;
+        option1.text = "";
+        option2.text = "";
+        if (option3_bt != null && option3_bt.activeSelf)
+        {
+            if (option3 != null)
+            {
+                option3.text = "";
+            }
+        }
+      
+        
+        
+        if (interobj == "옷 가게")
+        {
+            who.text = "system";
+            content.text = "벙거지를 구매했다.";
+            button.SetActive(true);
+            buttonnum = 0;
+
+
+        }
+        if (interobj == "편의점")
+        {
+            who.text = "system";
+            content.text = "과자를 구매했다.";
+            button.SetActive(true);
+            buttonnum = 0;
+
+
+        }
+        options.SetActive(false);
+    }
+    /*public void exitdown()
+    {
+        option1.text = "";
+        option2.text = "";
+        if (option3_bt != null && option3_bt.activeSelf)
+        {
+            if (option3 != null)
+            {
+                option3.text = "";
+            }
+
+        }
+        talksqu.SetActive(false);
+        buttonnum = 0;
+        interobj = null;
+    }*/
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.name == "basebody" || collision.gameObject.name == "Player")
@@ -100,14 +238,16 @@ public class npc : MonoBehaviour
         if(interobj=="델리만쥬 가게")
         {  
             Debug.Log(optnum+"");
-            if (optnum==1)
+            if (optnum==1&&manjufirst==0)
             {
               
                 if (buttonnum>0)
                 {
+                    manjufirst = 1;
                     talksqu.SetActive(false);
                     buttonnum = 0;
                     interobj = null;
+                    
                 }
                 else
                 {
@@ -117,11 +257,12 @@ public class npc : MonoBehaviour
                 }
                 
             }
-            if (optnum==2)
+            if (optnum==2&&manjufirst==0)
             {
                 Debug.Log(buttonnum);
                 if(buttonnum>3)
                 {
+                    manjufirst = 1;
                     talksqu.SetActive(false);
                     buttonnum = 0;
                     interobj = null;
@@ -144,12 +285,100 @@ public class npc : MonoBehaviour
                 }
                 
             }
+            if(manjufirst==1)
+            {
+                talksqu.SetActive(false);
+                buttonnum = 0;
+                interobj = null;
+                option1.text = "";
+                option2.text = "";
+                if (option3_bt != null && option3_bt.activeSelf)
+                {
+                    if (option3 != null)
+                    {
+                        option3.text = "";
+                    }
+                }
+            }
+            
           
           
         }
-      
-       
-     
+
+
+        if (interobj == "옷 가게")
+        {
+
+           
+            if (clofirst==0)
+            {
+                buttonnum++;
+                if (buttonnum > 2)
+                {
+                    talksqu.SetActive(false);
+                    buttonnum = 0;
+                    
+                    interobj = null;
+                    clofirst = 1;
+                }
+                else
+                {
+                    who.text = clocontent[buttonnum].Substring(0, 2);
+                    content.text = clocontent[buttonnum].Substring(2);
+
+                }
+            }
+            else
+            {
+                if(clofirst==1)
+                {
+                    talksqu.SetActive(false);
+                    buttonnum = 0;
+                    interobj = null;
+                    option1.text = "";
+                    option2.text = "";
+                    if (option3_bt != null && option3_bt.activeSelf)
+                    {
+                        if (option3 != null)
+                        {
+                            option3.text = "";
+                        }
+                    }
+                }
+            }
+
+
+
+        }
+        if (interobj == "편의점")
+        {
+
+
+            
+            talksqu.SetActive(false);
+            buttonnum = 0;
+            interobj = null;
+            option1.text = "";
+            option2.text = "";
+            if (option3_bt != null && option3_bt.activeSelf)
+            {
+                if (option3 != null)
+                {
+                    option3.text = "";
+                }
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
         if (interobj == "헛소리 하는 노인")
         {
             buttonnum++;
@@ -289,8 +518,18 @@ public class npc : MonoBehaviour
         if (playercolflag == 1 && gameObject.name == "델리만쥬 가게")
         {
             interobj = "델리만쥬 가게";
-
-            if (buttonnum == 0)
+            if (manjufirst == 1&& buttonnum==0)
+            {
+                talksqu.SetActive(true);
+                options.SetActive(true);
+                who.text = "system";
+                content.text = "무엇을 구매할까?";
+                option1.text = "> 델리만쥬";
+                option2.text = "> 핫도그";
+                button.SetActive(true);
+                //exit.text = "> 사지 않는다.";
+            }
+            else if (buttonnum == 0)
             {
                 talksqu.SetActive(true);
                 options.SetActive(true);
@@ -298,8 +537,63 @@ public class npc : MonoBehaviour
                 content.text = "........";
                 option1.text = "> 사 줄까요?";
                 option2.text = "> 돈 없으세요?";
+                option3_bt.SetActive(false);
                 button.SetActive(false);
             }
+
+
+
+        }
+
+        if (playercolflag == 1 && gameObject.name == "옷 가게")
+        {
+            interobj = "옷 가게";
+            Debug.Log(clofirst +"옷 플래그");
+            if (buttonnum == 0 && clofirst == 0)
+            {
+                
+                talksqu.SetActive(true);
+                options.SetActive(true);
+                who.text = clocontent[0].Substring(0, 2);
+                content.text = clocontent[0].Substring(2);
+            }
+            if (buttonnum==0&&clofirst != 0)
+
+            {
+                Debug.Log("들어옴");
+                talksqu.SetActive(true);
+                options.SetActive(true);
+                option3_bt.SetActive(true);
+                button.SetActive(false);
+                who.text = "system";
+                content.text = "무엇을 구매할까?";
+                option1.text = "> 짱구 잠옷";
+                option2.text = "> 요상한 맨투맨";
+                option3.text = "> 벙거지";
+                //exit.text = "> 사지 않는다.";
+            }
+
+
+
+        }
+
+        if (playercolflag == 1 && gameObject.name == "편의점")
+        {
+            interobj = "편의점";
+           
+            
+                Debug.Log("들어옴");
+                talksqu.SetActive(true);
+                options.SetActive(true);
+                option3_bt.SetActive(true);
+                button.SetActive(true);
+                who.text = "system";
+                content.text = "무엇을 구매할까?";
+                option1.text = "> 물";
+                option2.text = "> 보조배터리";
+                option3.text = "> 과자";
+                //exit.text = "> 사지 않는다.";
+
 
 
 
@@ -314,6 +608,7 @@ void Start()
         customize.sceneflag = 2;
         options.SetActive(false);
         button.SetActive(true);
+       
     }
 
     // Update is called once per frame

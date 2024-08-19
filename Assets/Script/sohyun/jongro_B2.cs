76fb32f8d3ebe9f74c9b13aebfd6b09f679f832b
sohyun-1122 <134932184+sohyun-1122@users.noreply.github.com>
2024-08-20 02:41:01 +0900

    종로3가 충돌처리

--- Assets/Script/sohyun/jongro_B2.cs
+++ Assets/Script/sohyun/jongro_B2.cs
@@ -7,10 +7,21 @@ public class jongro_B2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
-        GameObject upstair = GameObject.Find("[계단]_상단");
-        Debug.Log(upstair.transform.position.x);
-        Player.playertrans(upstair.transform.position.x, upstair.transform.position.y-3);
+       if(npc.B1toB2==1)
+        {
+            GameObject upstair = GameObject.Find("[계단]_상단");
+            Debug.Log(upstair.transform.position.x);
+            Player.playertrans(upstair.transform.position.x, upstair.transform.position.y - 5);
+            npc.B1toB2 = 0;
+        }
+       else if(npc.S3toB2==1)
+        {
+            GameObject upstair = GameObject.Find("[계단]_하단");
+            Debug.Log(upstair.transform.position.x);
+            Player.playertrans(upstair.transform.position.x, upstair.transform.position.y + 10);
+            npc.S3toB2 = 0;
+        }
+        
     }
 
     // Update is called once per frame

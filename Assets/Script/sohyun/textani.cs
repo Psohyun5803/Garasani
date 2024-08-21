abe8fee316fe1457a27fd0600aa2daf1bfbf2da9
sohyun-1122 <134932184+sohyun-1122@users.noreply.github.com>
2024-08-22 03:47:13 +0900

    1호선내부이벤트 (타이핑제외)

--- Assets/Script/sohyun/textani.cs
+++ Assets/Script/sohyun/textani.cs
@@ -4,27 +4,25 @@ using System.Collections;
 
 public class textani : MonoBehaviour
 {
-    public TMP_Text textComponent;  // TextMeshPro 텍스트 컴포넌트
-    public float typingSpeed = 0.05f;  // 타이핑 속도 조절
-
-    private string fullText;  // 전체 텍스트
+    public TextMeshProUGUI tmpText;
+    public float delay = 0.1f;
+    private string fullText;
+    private string currentText = "";
 
     void Start()
     {
-        if (textComponent != null)
-        {
-            fullText = textComponent.text;  // 텍스트 컴포넌트에서 전체 텍스트 가져오기
-            textComponent.text = "";  // 텍스트를 초기화하여 빈 상태로 설정
-            StartCoroutine(TypeText());  // 코루틴 시작
-        }
+        fullText = tmpText.text;
+        tmpText.text = "";
+        StartCoroutine(ShowText());
     }
 
-    IEnumerator TypeText()
+    IEnumerator ShowText()
     {
-        foreach (char letter in fullText)
+        for (int i = 0; i < fullText.Length; i++)
         {
-            textComponent.text += letter;  // 한 글자씩 텍스트에 추가
-            yield return new WaitForSeconds(typingSpeed);  // 타이핑 속도에 맞춰 대기
+            currentText = fullText.Substring(0, i + 1);
+            tmpText.text = currentText;
+            yield return new WaitForSeconds(delay);
         }
     }
 

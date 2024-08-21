4eb9e8656fd0e5a9fd1fd5d6c27541ba9ed1ab72
yeonee <leenayeon@sookmyung.ac.kr>
2024-08-21 22:57:35 +0900

    인벤토리 작업 중

--- Assets/Inven Script/DoubleClickButton.cs
+++ Assets/Inven Script/DoubleClickButton.cs
@@ -5,31 +5,39 @@ using System.Collections;
 
 public class DoubleClickToggleButton : MonoBehaviour
 {
-    public Button button; // UI ??
-    public Image buttonImage; // ?? ??? ????
-    public Sprite newSprite; // ??? ?? ???? ? ??? ???
-    private Sprite originalSprite; // ??? ?? ???
-    private int clickCount = 0; // ?? ??? ??
-    private float clickTime = 0; // ? ?? ??? ??? ??
-    private float clickDelay = 0.5f; // ?? ?? ?? (0.5?)
-    private bool isOriginal = true; // ?? ?? ???? ?? ????? ??
+    public Button button; // UI 버튼
+    //public Image buttonImage; // 버튼 이미지 컴포넌트
+    //public Sprite newSprite; // 버튼을 더블 클릭했을 때 변경될 이미지
+    //private Sprite originalSprite; // 버튼의 원래 이미지
+    private int clickCount = 0; // 클릭 횟수를 추적
+    private float clickTime = 0; // 첫 번째 클릭이 발생한 시간
+    private float clickDelay = 0.5f; // 더블 클릭 간격 (0.5초)
+    //private bool isOriginal = true; // 현재 버튼 이미지가 원래 이미지인지 여부
 
-    public TextMeshProUGUI itemNameText; // UI?? ??? ??? ??? TextMeshProUGUI ??
-    public TextMeshProUGUI itemInfoText; // UI?? ??? ??? ??? TextMeshProUGUI ??
-    public TextMeshProUGUI itemName; // ?? ??? ???? ??? ???? TextMeshProUGUI ??
-    public TextMeshProUGUI itemInfo; // ?? ??? ???? ??? ???? TextMeshProUGUI ??
+    public TextMeshProUGUI itemNameText; // 장착된 아이템 이름 
+    public TextMeshProUGUI itemInfoText; // 장착된 아이템 설명 
+    public TextMeshProUGUI itemName; // 현재 선택된 아이템 이름 
+    public TextMeshProUGUI itemInfo; // 현재 선택된 아이템 설명 
+    public GameObject[] item_quantity; // 아이템 수량 관리 
 
-    public GameObject[] item_quantity; // ?? ???? ??, ?????? ??? ??? ???? ? ?? ??
+    public static DoubleClickToggleButton instance;
 
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
 
 
     void Start()
     {
         button.onClick.AddListener(OnButtonClick);
-        originalSprite = buttonImage.sprite; // ?? ?? ???? ??
+        //originalSprite = buttonImage.sprite; // ?? ?? ???? ??
         for(int i = 0; i < 12; i++)
         {
-            item_quantity[i].SetActive(false); //?? ui ??? 
+            item_quantity[i].SetActive(false); 
         }
     }
 
@@ -43,21 +51,22 @@ public class DoubleClickToggleButton : MonoBehaviour
         }
         else if (clickCount == 2 && Time.time - clickTime < clickDelay)
         {
-            if (isOriginal)
-            {
-                buttonImage.sprite = newSprite; // ?? ???? ??
-                Debug.Log("???? ???????.");
-            }
-            else
-            {
-                buttonImage.sprite = originalSprite; // ?? ???? ???
-                Debug.Log("???? ?? ???? ???????.");
-            }
-            isOriginal = !isOriginal; // ??? ??? ??
+            //if (isOriginal)
+            //{
+            //    buttonImage.sprite = newSprite; // ?? ???? ??
+            //    Debug.Log("???? ???????.");
+            //}
+            //else
+            //{
+            //    buttonImage.sprite = originalSprite; // ?? ???? ???
+            //    Debug.Log("???? ?? ???? ???????.");
+            //}
+            //isOriginal = !isOriginal; // ??? ??? ??
             clickCount = 0;
 
             // ??? ?? ????
             UpdateItemInfo();
+
         }
         else
         {
--- Assets/Inven Script/Inventory.cs
+++ Assets/Inven Script/Inventory.cs
@@ -18,6 +18,7 @@ public struct Slot
     public TextMeshProUGUI itemText;
     public TextMeshProUGUI quantityText;
     public TextMeshProUGUI itemInfoText;
+    public bool isItem;
 }
 
 public class Inventory : MonoBehaviour
@@ -25,28 +26,50 @@ public class Inventory : MonoBehaviour
     public Item[] items = new Item[14];
     public Slot[] slots = new Slot[20];
 
+    public static Inventory instance;
 
-    public DoubleClickToggleButton toggleButton;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
 
     void Start()
     {
        
-
-        //??? ?? ??? 
-        items[0] = new Item { name = "????", info = "???" };
-        items[1] = new Item { name = "???", info = "????? ???? ?? ?" };
-        items[2] = new Item { name = "??? ??", info = "????? ???? ?? ?" };
-        items[3] = new Item { name = "???", info = "??? ???. ?? ??? ??? ??." };
-        items[4] = new Item { name = "??", info = "??? ??. ?? ??? ??? ??.)" };
-        items[5] = new Item { name = "?", info = "??? ??? ??? ??" };
-        items[6] = new Item { name = "????", info = "??? ????. ???? ?? ??? ??? ??." };
-        items[7] = new Item { name = "???", info = "????? ??? ???. ??? ??? ??? ?? ." };
-        items[8] = new Item { name = "???", info = "??? ????? ?? ???." };
-        items[9] = new Item { name = "???? ???", info = "?????? ? ??? ???." };
-        items[10] = new Item { name = "??", info = "?????? ? ??." };
-        items[11] = new Item { name = "? ???", info = "?????? ? ??." };
-        items[12] = new Item { name = "???", info = "?????? ? ???. ????? ????." };
-        items[13] = new Item { name = "???? ???", info = "?????? ? ??? ???." };
+        items[0] = new Item { name = "LED손전등", info = "어둠을 밝힐 수 있다." };
+        items[1] = new Item { name = "건전지", info = "건전지." };
+        items[2] = new Item { name = "교통카드", info = "개찰구에 찍어 세이브를 할 수 있다." };
+        items[3] = new Item { name = "구호물품", info = "역 내에 있는 구호물품보관함에 있던 것이다." };
+        items[4] = new Item { name = "김밥", info = "할머니에게서 산 김밥." };
+        items[5] = new Item { name = "낡은책", info = "50대 아저씨가 볼 법한 책이다." };
+        items[6] = new Item { name = "단소", info = "유명한 할아버지의 단소이다." };
+        items[7] = new Item { name = "델리만쥬", info = "달달한 델리만쥬. 체력, 정신력 회복에 도움이 된다." };
+        items[8] = new Item { name = "델리만쥬 봉투", info = "델리만쥬가 들어있는 봉투이다." };
+        items[9] = new Item { name = "물", info = "마시면 정신력 회복에 도움이 된다." };
+        items[10] = new Item { name = "반짝고글", info = "패션용 고글." };
+        items[11] = new Item { name = "보조배터리", info = "핸드폰 충전에 사용할 수 있다." };
+        items[12] = new Item { name = "복권", info = "인생 한방을 노려보자." };
+        items[13] = new Item { name = "비상망치", info = "비상용 망치이다." };
+        items[14] = new Item { name = "비상조명등", info = "비상용 조명." };
+        items[15] = new Item { name = "사다리", info = "사다리." };
+        items[16] = new Item { name = "소방도끼", info = "소방용 도끼." };
+        items[17] = new Item { name = "소주", info = "취하고 싶을 때 마셔보자." };
+        items[18] = new Item { name = "신문지", info = "언제 신문일까?" };
+        items[19] = new Item { name = "에어팟한쪽", info = "누군가 두고 내린 에어팟 한쪽." };
+        items[20] = new Item { name = "움직이는강아지", info = "잡상인에게 산 귀여운 강아지 장난감이다." };
+        items[21] = new Item { name = "이온음료", info = "마시면 체력, 정신력 회복에 도움이 된다." };
+        items[22] = new Item { name = "찐옥수수", info = "할머니에게서 산 옥수수." };
+        items[23] = new Item { name = "찢겨진부적", info = "영문을 알 수 없는 글씨가 쓰여진 종이. 섬뜩하게 찢겨져있다." };
+        items[24] = new Item { name = "캔커피", info = "마시면 정신력 회복에 도움이 된다." };
+        items[25] = new Item { name = "키토산파스", info = "붙이면 체력 회복에 도움이 된다." };
+        items[26] = new Item { name = "탄산음료1", info = "마시면 정신력 회복에 도움이 된다." };
+        items[27] = new Item { name = "탄산음료2", info = "마시면 정신력 회복에 도움이 된다." };
+        items[28] = new Item { name = "탄산음료3", info = "마시면 정신력 회복에 도움이 된다." };
+        items[29] = new Item { name = "풀페이스두건", info = "패션용 두건." };
 
 
         // ?? ?? ??? 
@@ -65,21 +88,23 @@ public class Inventory : MonoBehaviour
     {
         bool itemAdded = false;
 
-        // ??? ?? ??? ??? ?? 
+        //add item 
         for (int i = 0; i < slots.Length; i++)
         {
             if (slots[i].itemName == newItemName)
             {
                 slots[i].quantity++;
                 slots[i].quantityText.text = slots[i].quantity.ToString();
-                Debug.Log($"?? ??? ?? ?? ??" + slots[i].quantity);
-                toggleButton.UpdateQuantity(slots[i].quantity);
+                DoubleClickToggleButton.instance.UpdateQuantity(slots[i].quantity); //수량 업데이트
+                
                 itemAdded = true;
+                slots[i].isItem = true;
+                item_setActive.instance.updateItem(slots[i].itemName);
                 break;
             }
         }
 
-        // ? ??? ??? ??? ??
+        // new item 
         if (!itemAdded)
         {
             for (int i = 0; i < slots.Length; i++)
@@ -92,8 +117,9 @@ public class Inventory : MonoBehaviour
                     slots[i].itemText.text = newItemName;
                     slots[i].quantityText.text = "1";
                     slots[i].itemInfoText.text = newItemInfo;
-                    toggleButton.UpdateQuantity(slots[i].quantity);
-                    Debug.Log("?? ? ??? ??" + slots[i].itemName);
+                    DoubleClickToggleButton.instance.UpdateQuantity(slots[i].quantity); //수량 업데이트
+
+                    item_setActive.instance.updateItem(slots[i].itemName);
                     break;
                 }
             }

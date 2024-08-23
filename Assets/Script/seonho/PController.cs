e14cee1d0f833d37be523739d4b634074f03ce2c
zxcv0028 <bad020906@gmail.com>
2024-08-23 23:47:56 +0900

    1호선 수정

---
+++ Assets/Script/seonho/PController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PController : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+
+        if (player != null)
+        {
+            player.transform.position = Vector3.zero;
+            Debug.Log("플레이어 위치가 (0,0,0)으로 이동되었습니다.");
+        }
+        else
+        {
+            Debug.LogError("Player라는 이름의 오브젝트를 찾을 수 없습니다.");
+        }
+    }
+}

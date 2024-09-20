2a785c17ce37b46a58eab51a39ecdcbfc7076398
zxcv0028 <bad020906@gmail.com>
2024-09-21 03:12:07 +0900

    수정

--- Assets/Script/seonho/Jongro/Sub1_PlayerPosition.cs
+++ Assets/Script/seonho/Jongro/Sub1_PlayerPosition.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sta1_PlayerPosition1 : MonoBehaviour
+public class Sub1_PlayerPosition : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        GameObject upstair = GameObject.Find("1호선계단"); 
+        GameObject upstair = GameObject.Find("1호선계단");
         Player.playertrans(upstair.transform.position.x + 5, upstair.transform.position.y);
     }
 

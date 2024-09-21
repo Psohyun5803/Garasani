2471506bef4097fdda9bd14e716acdba63aee2bd
zxcv0028 <bad020906@gmail.com>
2024-09-21 17:11:57 +0900

    종로 수정

--- Assets/Script/seonho/Jongro/Con_PlayerPosition1.cs
+++ Assets/Script/seonho/Jongro/Con_PlayerPosition1.cs
@@ -9,6 +9,7 @@ public class Con_PlayerPosition1 : MonoBehaviour
     {
         GameObject upstair = GameObject.Find("ÀÚÆÇ±â01"); 
         Player.playertrans(upstair.transform.position.x, upstair.transform.position.y -5);
+        Player.moveflag = 1;
     }
 
     // Update is called once per frame

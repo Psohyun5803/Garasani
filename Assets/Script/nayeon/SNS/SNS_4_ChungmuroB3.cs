4f5b7d7781e7d828f665c88e69d2f7437999d711
sohyun-1122 <161833145+Psohyun5803@users.noreply.github.com>
2025-02-08 15:24:32 +0900

    충무로 wiki

--- Assets/Script/nayeon/SNS/SNS_4_ChungmuroB3.cs
+++ Assets/Script/nayeon/SNS/SNS_4_ChungmuroB3.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SNS_4_ChungmuroB3 : MonoBehaviour
 {
-    public GameObject twit_horror;
+    public GameObject whole;
 
     // Start is called before the first frame update
     void Start()
     {
-        twit_horror.SetActive(true);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (whole.activeSelf)
+        {
+            foreach (Transform child in whole.transform)
+            {
+                if (child.CompareTag("sns_prol"))  // 특정 태그가 달린 자식인지 확인
+                {
+                    child.gameObject.SetActive(true);  // 해당 자식만 활성화
+                    Debug.Log("활성됨");
+                }
+                else
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
     }
 }
